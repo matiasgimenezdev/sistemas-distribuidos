31101791f0c54b4fc468e48ceb6333fd012d32bd
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-03-29 17:40:56 -0300

    Agrega pull de imagen docker

--- practica1/7-GenericTask-Matias/service/src/main/java/com/example/service/Service.java
+++ practica1/7-GenericTask-Matias/service/src/main/java/com/example/service/Service.java
@@ -6,26 +6,56 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.json.JSONException;
 import org.json.JSONObject;
 import com.google.gson.Gson;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Image;
+import com.github.dockerjava.core.command.PullImageResultCallback;
 
 @RestController
 @SpringBootApplication
 public class Service {
+	private DockerClient dockerClient;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Service.class, args);
 	}
 
 	@PostMapping("/task")
-	public ResponseEntity<String> getRequest(@RequestBody String body) {
+	public ResponseEntity<String> postRequest(@RequestBody String body) {
 		try{
 			JSONObject params = new JSONObject(body);
 			JSONObject response = ejecutarTarea(params);
 			return ResponseEntity.ok(response.toString());
 		} catch(JSONException e){
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("JSON Error: " + e.getMessage());
+		} catch (Exception e){
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor: " + e.getMessage());
+		}
+	}
+
+	@GetMapping("test")
+	public ResponseEntity<String> test(){
+		try{
+			this.dockerClient = DockerClientBuilder.getInstance().build();
+			JSONObject response = new JSONObject();
+			List<Image> imageListBefore = dockerClient.listImagesCmd().exec();
+			response.put("before", imageListBefore);
+			boolean bool = dockerClient.pullImageCmd("hello-world")
+			.withTag("latest")
+			.exec(new PullImageResultCallback())
+			.awaitCompletion(100, TimeUnit.SECONDS);
+			
+			List<Image> imageListAfter = dockerClient.listImagesCmd().exec();
+			response.put("after", imageListAfter);
+
+			return ResponseEntity.ok(response.toString());
 		} catch (Exception e){
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor: " + e.getMessage());
 		}
@@ -40,5 +70,6 @@ public class Service {
 		response.put("result", randomNumber.toString());
 		return response;
 	}
-
 }
+
+
