9939a8b6aa5b9991bd1034f552ca94a2a61380a7
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-03-31 15:08:11 -0300

    Usa la clase HttpClient para la request y agrega el parametro 'path'

--- practica1/7-GenericTask/client/src/main/java/com/client/Client.java
+++ practica1/7-GenericTask/client/src/main/java/com/client/Client.java
@@ -1,74 +1,56 @@
 package com.client;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import org.json.JSONObject;
-
 import java.util.Scanner;
-import java.util.function.IntToDoubleFunction;
 
 public class Client {
     public static void main(String[] args) {
         try {
-            URL url = new URL("http://localhost:8080/remotetask");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-
-            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-
             Scanner sc = new Scanner(System.in);
 
-            System.out.println("");
-            System.out.println("Se ha conectado al servidor correctamente: ");
-            System.out.println("Ingrese un valor mínimo: ");
-            Double min = (double) sc.nextInt();
-            System.out.println("Ingrese un valor máximo: ");
-            Double max = (double) sc.nextInt();
+            System.out.print("# Ingrese un valor mínimo: ");
+            Double min = sc.nextDouble();
+            System.out.print("# Ingrese un valor máximo: ");
+            Double max = sc.nextDouble();
             System.out.println("");
 
             JSONObject body = new JSONObject();
-            body.put("task", "randomNumber");
             body.put("dockerImage", "mgimenezdev/task-service:v1");
             body.put("dockerImageTag", "v1");
             body.put("dockerContainerName", "task-service");
-            body.put("port", "5000");
+            body.put("port", 5000);
+            body.put("path", "/task");
             body.put("min", min);
             body.put("max", max);
-            out.write(body.toString());
-            out.flush();
-            out.close();
 
-            System.out.println("Enviando datos al servidor...");
-            System.out.println("");
-            System.out.println("Tarea: randomNumber");
-            System.out.println("Imagen: mgimenezdev/task-service:v1");
-            System.out.println("Tag: v1");
-            System.out.println("Nombre de Contenedor: task-service");
-            System.out.println("Puerto : 5000");
-            System.out.println("Mínimo: " + min);
-            System.out.println("Máximo: " + max);
-            System.out.println("");
-            System.out.println("Resolviendo la tarea...");
-            System.out.println("");
+            HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder()
+									.uri(URI.create("http://localhost:8080/remotetask"))
+									.POST(HttpRequest.BodyPublishers.ofString(body.toString()))
+									.build();
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-
-            in.close();
-            JSONObject responseAsJson = new JSONObject(response.toString());
-            Double resultado = responseAsJson.getDouble("result");
-            System.out.println("Numero aleatorio: " + resultado.toString());
+            System.out.println("# Enviando datos al servidor: ");
+            System.out.println("- Tarea: Calcular numero aleatorio");
+            System.out.println("- Imagen Docker: mgimenezdev/task-service:v1");
+            System.out.println("- Tag: v1");
+            System.out.println("- Nombre de Contenedor: task-service");
+            System.out.println("- Puerto : 5000");
+            System.out.println("- Mínimo: " + min);
+            System.out.println("- Máximo: " + max);
+            System.out.println("");
+            System.out.println("# Resolviendo la tarea...");
             System.out.println("");
 
+            HttpResponse<String> response = null;
+			response = client.send(request,
+                        HttpResponse.BodyHandlers.ofString());
+            JSONObject responseAsJson = new JSONObject(response.body().toString());
+            Double result = responseAsJson.getDouble("result");
+            System.out.println("Numero aleatorio: " + result.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
