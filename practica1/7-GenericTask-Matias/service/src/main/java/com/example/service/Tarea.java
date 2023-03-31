0fb296844378cd64b736ddd027903ca5f6ba0bdd
SimonDiLeoGIT <saimondiley@gmail.com>
2023-03-30 23:41:05 -0300

    Agrega Menú a Cliente

--- practica1/7-GenericTask-Matias/service/src/main/java/com/example/service/Tarea.java
+++ practica1/7-GenericTask-Matias/service/src/main/java/com/example/service/Tarea.java
@@ -3,14 +3,14 @@ package com.example.service;
 import org.json.JSONObject;
 
 public class Tarea {
-    
+
     public JSONObject ejecutar(JSONObject params) {
         JSONObject result = new JSONObject();
         Double min = params.getDouble("min");
         Double max = params.getDouble("max");
-        Double randomNumber = Math.ceil(min + (Math.random() * max)); 
+        double randomNumber = Math.random() * (max - min) + min;
         result.put("result", randomNumber);
         return result;
     }
-    
+
 }
