3bfd92ba7d5a4e2edce0ebbc57e789cf95f145ac
SaimonDiley <simodileo01@gmail.com>
2023-04-13 10:43:46 -0300

    Agrega archivo de configuración a EndNode y logra conección a Master 9000

--- practica2/1-P2PNetwork/endnode/src/main/java/com/example/App.java
+++ practica2/1-P2PNetwork/endnode/src/main/java/com/example/App.java
@@ -1,12 +1,14 @@
 package com.example;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
         EndNode en = new EndNode();
--- practica2/1-P2PNetwork/endnode/src/main/java/com/example/EndNode.java
+++ practica2/1-P2PNetwork/endnode/src/main/java/com/example/EndNode.java
@@ -2,18 +2,49 @@ package com.example;
 
 import java.net.*;
 import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EndNode {
     
-    private static final String SERVER_ADDRESS = "localhost";
-    private static final int SERVER_PORT = 9000;
+    private static final String SERVER_CONFIG_FILE = "server.properties";
+    private final List<String> masterAddresses;
+    private final List<Integer> masterPorts;
+
+    public EndNode() throws IOException {
+        // Initialize master addresses and ports from config file
+        this.masterAddresses = new ArrayList<>();
+        this.masterPorts = new ArrayList<>();
+        File configFile = new File(SERVER_CONFIG_FILE);
+        BufferedReader reader = new BufferedReader(new FileReader(configFile));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(":");
+            masterAddresses.add(parts[0]);
+            masterPorts.add(Integer.parseInt(parts[1]));
+        }
+        reader.close();
+    }
 
     public void start() {
         try {
-            // Conectar como cliente al servidor centralizado
-            Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-            System.out.println("Conectado al servidor centralizado");
             
+            Socket socket = new Socket(masterAddresses.get(0), masterPorts.get(0));
+                
+            System.out.println("Conectado al nodo maestro: " + masterAddresses.get(0));
+            
+            // Realizar consultas al servidor centralizado
+        
             // Realizar consultas al servidor centralizado
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
@@ -25,23 +56,6 @@ public class EndNode {
             response = reader.readLine();
             System.out.println("Respuesta del servidor centralizado: " + response);
 
-            // Atender solicitudes de otros nodos extremos
-            ServerSocket serverSocket = new ServerSocket(9001);
-            System.out.println("Servidor de nodo extremo iniciado");
-
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Nodo extremo conectado: " + clientSocket.getInetAddress().getHostName());
-                
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                String request = in.readLine();
-                System.out.println("Solicitud recibida: " + request);
-                // Procesar la solicitud y enviar una respuesta
-                String responseMsg = "Respuesta a solicitud de " + clientSocket.getInetAddress().getHostName();
-                out.println(responseMsg);
-            }
 
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
