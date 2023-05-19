5cc496271ba4e933e435425da803a17bd3214568
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-05-19 19:30:47 -0300

    Agrega division de imagen

---
+++ practica2/2-Sobel/DistributedSobel/SplitService/src/main/java/com/sd/java/sobel/SobelApplication.java
@@ -0,0 +1,12 @@
+package com.sd.java.sobel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SplitController.class, args);
+  }
+}
