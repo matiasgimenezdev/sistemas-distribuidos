4e6cb128b1a6cad5698b9844d4347c3186b3b5a8
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-05-20 12:50:20 -0300

    Fixing start bug

--- practica2/2-Sobel/DistributedSobel/MessageQueue/src/main/java/com/sd/sobel/App.java
+++ practica2/2-Sobel/DistributedSobel/MessageQueue/src/main/java/com/sd/sobel/App.java
@@ -2,9 +2,13 @@ package com.sd.sobel;
 
 import com.sd.sobel.controllers.TasksController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class App {
 
   public static void main(String[] args) {
