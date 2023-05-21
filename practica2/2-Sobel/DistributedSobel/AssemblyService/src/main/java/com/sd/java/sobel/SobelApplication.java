2fadd32d856732eec4f01b68c449003b15b53979
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-05-21 11:35:37 -0300

    Adding spring project for image assembly service

---
+++ practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/SobelApplication.java
@@ -0,0 +1,13 @@
+package com.sd.java.sobel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobelApplication.class, args);
+	}
+
+}
