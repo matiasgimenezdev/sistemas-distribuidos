5463bfc8d21299646dd7cc85fd8813c78afad3c2
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-05-22 12:59:17 -0300

    postgres integration + task register in bdd

--- practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/controllers/TaskRegisterController.java
+++ practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/controllers/TaskRegisterController.java
@@ -2,6 +2,7 @@ package com.sd.java.sobel.controllers;
 
 import com.sd.java.sobel.services.DatabaseConnection;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import org.json.JSONObject;
@@ -30,23 +31,39 @@ public class TaskRegisterController {
     try {
       connection = this.databaseConnection.getConnection();
       Statement statement = connection.createStatement();
+      // TODO -> VERIFICAR QUE NO EXISTA EL TASK_ID ANTES DE INSERTAR
       String query =
-        "INSERT INTO TASKS (TASK_ID, PARTS, WIDTH, HEIGHT, SOURCE, DESTINATION) values (" +
+        "INSERT INTO TASKS (TASK_ID, PARTS, WIDTH, HEIGHT, SOURCE, DESTINATION)  values ('" +
         taskData.getString("taskId") +
-        ", " +
+        "', '" +
         taskData.getString("parts") +
-        ", " +
+        "', '" +
         taskData.getString("width") +
-        ", " +
+        "', '" +
         taskData.getString("height") +
-        ", " +
+        "', '" +
         taskData.getString("source") +
-        ", " +
+        "', '" +
         taskData.getString("destination") +
-        ")";
+        "')";
       int rowsAffected = statement.executeUpdate(query);
       if (rowsAffected > 0) {
         System.out.println("Tarea registrada en la base de datos.");
+        String readQuery =
+          "SELECT * FROM TASKS WHERE TASK_ID = '" +
+          taskData.getString("taskId") +
+          "'";
+        ResultSet resultSet = statement.executeQuery(readQuery);
+        if (resultSet.next()) {
+          System.out.println("TASK_ID: " + resultSet.getString("TASK_ID"));
+          System.out.println("PARTS: " + resultSet.getString("PARTS"));
+          System.out.println("WIDTH: " + resultSet.getString("WIDTH"));
+          System.out.println("HEIGHT: " + resultSet.getString("HEIGHT"));
+          System.out.println("SOURCE: " + resultSet.getString("SOURCE"));
+          System.out.println(
+            "DESTINATION: " + resultSet.getString("DESTINATION")
+          );
+        }
       } else {
         System.out.println("Error al registrar la tarea en la base de datos");
       }
