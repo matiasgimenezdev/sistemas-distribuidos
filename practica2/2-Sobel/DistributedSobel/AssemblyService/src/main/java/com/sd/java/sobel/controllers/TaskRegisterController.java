76ef32bc1cf9d098e78f89a781bda9e618fe4307
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-05-22 11:55:26 -0300

    Comienza con integracion de postgres

--- practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/controllers/TaskRegisterController.java
+++ practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/controllers/TaskRegisterController.java
@@ -1,6 +1,11 @@
 package com.sd.java.sobel.controllers;
 
+import com.sd.java.sobel.services.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +15,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TaskRegisterController {
 
+  private DatabaseConnection databaseConnection;
+
+  @Autowired
+  public TaskRegisterController(DatabaseConnection databaseConnection) {
+    this.databaseConnection = databaseConnection;
+    databaseConnection.createTableIfNotExists();
+  }
+
   @PostMapping("/register")
   public ResponseEntity<String> taskRegister(@RequestBody String body) {
-    System.out.println("Saving REGISTER TASK in BDD : " + body);
     JSONObject taskData = new JSONObject(body);
+    Connection connection = null;
     try {
-      // Guardar tarea (taskData) en BDD.
+      connection = this.databaseConnection.getConnection();
+      Statement statement = connection.createStatement();
+      String query =
+        "INSERT INTO TASKS (TASK_ID, PARTS, WIDTH, HEIGHT, SOURCE, DESTINATION) values (" +
+        taskData.getString("taskId") +
+        ", " +
+        taskData.getString("parts") +
+        ", " +
+        taskData.getString("width") +
+        ", " +
+        taskData.getString("height") +
+        ", " +
+        taskData.getString("source") +
+        ", " +
+        taskData.getString("destination") +
+        ")";
+      int rowsAffected = statement.executeUpdate(query);
+      if (rowsAffected > 0) {
+        System.out.println("Tarea registrada en la base de datos.");
+      } else {
+        System.out.println("Error al registrar la tarea en la base de datos");
+      }
       return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      e.printStackTrace();
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    } finally {
+      // Asegurarse de cerrar la conexión en caso de excepción
+      if (connection != null) {
+        try {
+          connection.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
     }
   }
 }
---
+++ practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/services/DatabaseConnection.java
@@ -0,0 +1,91 @@
+package com.sd.java.sobel.services;
+
+import java.sql.*;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseConnection {
+
+  private String url;
+
+  private String username;
+
+  private String password;
+
+  private String dbname;
+
+  private Boolean tableExists;
+
+  public DatabaseConnection(Environment environment) {
+    this.url = environment.getProperty("postgres.url", "URL not found");
+    this.username =
+      environment.getProperty("postgres.username", "USERNAME not found");
+    this.password =
+      environment.getProperty("postgres.password", "PASSWORD not found");
+    this.dbname =
+      environment.getProperty("postgres.dbname", "DBNAME not found");
+    this.tableExists = false;
+    try (
+      Connection connection = DriverManager.getConnection(
+        this.url + "postgres",
+        this.username,
+        this.password
+      )
+    ) {
+      this.url = url + dbname;
+      Statement statement = connection.createStatement();
+
+      // Verificar si la base de datos existe
+      ResultSet resultSet = statement.executeQuery(
+        "SELECT 1 FROM pg_database WHERE datname='" + this.dbname + "'"
+      );
+      if (!resultSet.next()) {
+        // Crea la BDD 'task-db' si no existe
+        statement.executeUpdate("CREATE DATABASE " + this.dbname);
+        System.out.println("Se creó la base de datos '" + dbname + "'");
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Connection getConnection() {
+    try {
+      return DriverManager.getConnection(url, username, password);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public void createTableIfNotExists() {
+    try (
+      Connection connection = DriverManager.getConnection(
+        url,
+        username,
+        password
+      )
+    ) {
+      Statement statement = connection.createStatement();
+
+      ResultSet resultSet = statement.executeQuery(
+        "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'TASKS')"
+      );
+      resultSet.next();
+      boolean tableExists = resultSet.getBoolean(1);
+
+      if (!tableExists) {
+        // Crea la tabla 'TASKS' si no existe
+        statement.executeUpdate(
+          "CREATE TABLE TASKS (TASK_ID char(60) PRIMARY KEY, PARTS VARCHAR(50), WIDTH VARCHAR(10), HEIGHT VARCHAR(10), SOURCE VARCHAR(255), DESTINATION VARCHAR(255))"
+        );
+        System.out.println("Se creó la tabla 'TASKS'");
+      } else {
+        System.out.println("La tabla 'TASKS' ya existe");
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+}
