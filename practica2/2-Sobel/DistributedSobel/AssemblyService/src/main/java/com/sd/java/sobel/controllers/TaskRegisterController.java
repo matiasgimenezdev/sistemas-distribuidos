0236891ef989c36366c2f066fc90b7fe30073f67
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-05-22 23:00:22 -0300

    Fix task insertion

--- practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/controllers/TaskRegisterController.java
+++ practica2/2-Sobel/DistributedSobel/AssemblyService/src/main/java/com/sd/java/sobel/controllers/TaskRegisterController.java
@@ -1,10 +1,7 @@
 package com.sd.java.sobel.controllers;
 
 import com.sd.java.sobel.services.DatabaseConnection;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,41 +28,39 @@ public class TaskRegisterController {
     try {
       connection = this.databaseConnection.getConnection();
       Statement statement = connection.createStatement();
-      // TODO -> VERIFICAR QUE NO EXISTA EL TASK_ID ANTES DE INSERTAR
-      String query =
-        "INSERT INTO TASKS (TASK_ID, PARTS, WIDTH, HEIGHT, SOURCE, DESTINATION)  values ('" +
-        taskData.getString("taskId") +
-        "', '" +
-        taskData.getString("parts") +
-        "', '" +
-        taskData.getString("width") +
-        "', '" +
-        taskData.getString("height") +
-        "', '" +
-        taskData.getString("source") +
-        "', '" +
-        taskData.getString("destination") +
-        "')";
-      int rowsAffected = statement.executeUpdate(query);
-      if (rowsAffected > 0) {
-        System.out.println("Tarea registrada en la base de datos.");
-        String readQuery =
-          "SELECT * FROM TASKS WHERE TASK_ID = '" +
-          taskData.getString("taskId") +
-          "'";
-        ResultSet resultSet = statement.executeQuery(readQuery);
-        if (resultSet.next()) {
-          System.out.println("TASK_ID: " + resultSet.getString("TASK_ID"));
-          System.out.println("PARTS: " + resultSet.getString("PARTS"));
-          System.out.println("WIDTH: " + resultSet.getString("WIDTH"));
-          System.out.println("HEIGHT: " + resultSet.getString("HEIGHT"));
-          System.out.println("SOURCE: " + resultSet.getString("SOURCE"));
+
+      String query = "SELECT * FROM TASKS WHERE TASK_ID = ?";
+      PreparedStatement preparedStatement = connection.prepareStatement(query);
+      preparedStatement.setString(1, taskData.getString("taskId"));
+      ResultSet resultSet = preparedStatement.executeQuery();
+      if (!resultSet.next()) {
+        query =
+          "INSERT INTO TASKS (TASK_ID, PARTS, WIDTH, HEIGHT, SOURCE, DESTINATION) VALUES (?, ?, ?, ?, ?, ?)";
+
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, taskData.getString("taskId"));
+        preparedStatement.setString(2, taskData.getString("parts"));
+        preparedStatement.setString(3, taskData.getString("width"));
+        preparedStatement.setString(4, taskData.getString("height"));
+        preparedStatement.setString(5, taskData.getString("source"));
+        preparedStatement.setString(6, taskData.getString("destination"));
+
+        int rowsAffected = preparedStatement.executeUpdate();
+        if (rowsAffected > 0) {
+          System.out.println("Tarea registrada en la base de datos.");
+          System.out.println("TASK_ID: " + taskData.getString("taskId"));
+          System.out.println("PARTS: " + taskData.getString("parts"));
+          System.out.println("WIDTH: " + taskData.getString("width"));
+          System.out.println("HEIGHT: " + taskData.getString("height"));
+          System.out.println("SOURCE: " + taskData.getString("source"));
           System.out.println(
-            "DESTINATION: " + resultSet.getString("DESTINATION")
+            "DESTINATION: " + taskData.getString("destination")
           );
+        } else {
+          System.out.println("Error al registrar la tarea en la base de datos");
         }
       } else {
-        System.out.println("Error al registrar la tarea en la base de datos");
+        System.out.println("Ya existe un registro con esa clave primaria");
       }
       return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e) {
